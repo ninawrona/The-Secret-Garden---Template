8cef255e22bb7f203577c18b32c0ddcae347b877
ninawrona <90191093+ninawrona@users.noreply.github.com>
2021-11-22 13:57:04 +0100

    applesChanges

--- The Secret Garden - Template/js/script.js
+++ The Secret Garden - Template/js/script.js
@@ -1,1 +1,24 @@
 //MAKE THE MAGIC HAPPEN
+
+var yVar = $("#basket").offset().top();
+var xVar = $("#basket").offset().left();
+
+function randomX(){
+var positionX = Math.floor(Math.random()*($(window).width()-$(".apple").width()));
+return positionX;
+}
+
+function randomY(){
+    var positionY = Math.floor(Math.random()*($(window).height()-$(".apple").height()));
+return positionY;
+}
+
+$(document).ready(function(){
+    $("#apple1").offset({top: randomY(), left: randomX()});
+    $("#apple2").offset({top: randomY(), left: randomX()});
+    $("#apple3").offset({top: randomY(), left: randomX()});
+});
+
+$(".apple").click(function(){
+    $(this).animate({top: yVar, left: xVar});
+})
