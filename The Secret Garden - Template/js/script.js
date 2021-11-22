7c92b40a53825974430c8797d10699231a55f9d1
MartyBobo <martin.rosendahl4@gmail.com>
2021-11-22 13:22:17 +0100

    problem 1 and  2

--- The Secret Garden - Template/js/script.js
+++ The Secret Garden - Template/js/script.js
@@ -1,1 +1,42 @@
 //MAKE THE MAGIC HAPPEN
+
+var butterflyX;
+var butterflyY;
+var windowWidth = $(window).width() - $("#butterfly").width();
+var windowHeight= $(window).height()- $("#butterfly").height();
+var randomX;
+var randomY;
+
+
+
+
+function randomXY(){
+ butterflyX = Math.floor(Math.random()*windowWidth);
+ butterflyY = Math.floor(Math.random()*windowHeight);
+}
+
+
+
+$("#butterfly").animate( "slow", function() {squareMovement(this)});
+
+$("#butterfly").on("mouseenter",function(){
+    randomXY();
+    $("#butterfly").animate({top: butterflyY, left: butterflyX}, "slow");
+    $("#butterfly").stop();
+
+
+});
+
+
+function squareMovement(IdRef) {
+    randomXY();
+    $(IdRef).animate({top: butterflyY}).delay(2000).animate({left: butterflyX}, "slow", function(){squareMovement(IdRef);});
+}
+
+
+$(window).on("mouseenter", function(event){
+    var xCoord = event.pageX;
+    var yCoord = event.pageY;
+
+    $("#net").offset({top: yCoord, left: xCoord});
+});
