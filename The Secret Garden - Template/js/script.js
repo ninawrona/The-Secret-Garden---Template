eeb3964d7a99153cc29d76e68dab63290cf89d5d
KamilFischbach <92295061+KamilFischbach@users.noreply.github.com>
2021-11-22 14:42:49 +0100

    SunSoFar

--- The Secret Garden - Template/js/script.js
+++ The Secret Garden - Template/js/script.js
@@ -14,6 +14,7 @@ var yCoord;
 
 
 
+
 function randomXY(){
  butterflyX = Math.floor(Math.random()*windowWidth);
  butterflyY = Math.floor(Math.random()*windowHeight);
@@ -37,5 +38,27 @@ function movement(IdRef) {
 
 
 $(window).on("mousemove", function(event){
-    $("#net").offset({top: event.pageY, left: event.pageX});
+    $("#net").offset({top: event.pageY, left: event.pageX},);
+    $("#butterfly").css("z-index", "4");
 });
+
+$("#moon").hide();
+
+var windowHeightSunMoon = ($(window).height())/64;
+var windowWidhtSunMoon = ($(window).width())/2 -100;
+var windowWidhtSunMoonEnd = ($(window).width());
+
+
+
+$("#sun").animate({top: -150, left: windowWidhtSunMoon},10000,"linear",
+    function(){
+        $("#sun").css("z-index", "2");
+        $("#tree").css("z-index", "3");
+        $("#sun").animate({top: 150, left: windowWidhtSunMoonEnd }, 10000, "linear");
+
+
+} );
+
+
+
+
