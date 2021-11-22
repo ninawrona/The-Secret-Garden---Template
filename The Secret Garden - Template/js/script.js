8c61135bafdbdc2c8f23e7ea91b95b0a26236b71
ninawrona <90191093+ninawrona@users.noreply.github.com>
2021-11-22 15:14:36 +0100

    Apples to basket

--- The Secret Garden - Template/js/script.js
+++ The Secret Garden - Template/js/script.js
@@ -39,3 +39,35 @@ function movement(IdRef) {
 $(window).on("mousemove", function(event){
     $("#net").offset({top: event.pageY, left: event.pageX});
 });
+
+
+function appleRandomX(){
+    var appleX = Math.floor(Math.random()*372) + 718;
+    return appleX;
+}
+
+function appleRandomY(){
+    var appleY = Math.floor(Math.random()*172) + 178;
+    return appleY;
+}
+
+$(document).ready(function(){
+    $("#apple1").offset({top: appleRandomY(), left: appleRandomX()})
+    $("#apple2").offset({top: appleRandomY(), left: appleRandomX()})
+    $("#apple3").offset({top: appleRandomY(), left: appleRandomX()})
+})
+
+var basketY = $(".basket").offset().top;
+var basketX = $(".basket").offset().left;
+
+$("#apple1").on("click", function(){
+    $("#apple1").animate({top: basketY, left: basketX})
+})
+
+$("#apple2").on("click", function(){
+    $("#apple2").animate({top: basketY, left: basketX})
+})
+
+$("#apple3").on("click", function(){
+    $("#apple3").animate({top: basketY, left: basketX})
+})
